ce7eceabb80364e32d59ddcea5f653165bb7e1ec
OlgaTikhonchik <olga882011@gmail.com>
2023-12-11 18:16:50 +0200

    add modal script

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelectorAll('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+  
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function openModal(item) {item.addEventListener('click', toggleModal)};
+    refs.openModalBtn.forEach(openModal);
+    
+    
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
